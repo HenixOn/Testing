bd4720ef5b3f7810e807d5a8531098e70152905c
Henix Henixovich <herafedazrp@gmail.com>
2019-04-07 12:00:23 +0300

    Это наш первый комит

---
+++ Javascript/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let inc = 10,
+    decr = 10;
+
+console.log(inc++);
+console.log(decr--);
+console.log(5%2);
+
+
+
+let isChecked = true,
+    isClose = true;
+console.log(isChecked || isClose);
